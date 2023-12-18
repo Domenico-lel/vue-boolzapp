1f39894ee5f3d81ffee05fa84ccec8c9ad8c82f5
Domenico-lel <domenicolella2@gmail.com>
2023-12-18 11:19:30 +0100

    blocco chat dinamico

---
+++ script.js
@@ -0,0 +1,30 @@
+
+new Vue({
+    el: '#cont_green',
+    data: {
+      contacts: [
+        {
+          name: 'Michele',
+          avatar: 'img/avatar_1.jpg',
+          lastMessage: 'Hai portato a spasso il cane?',
+        },
+        {
+          name: 'Fabio',
+          avatar: 'img/avatar_2.jpg',
+          lastMessage: 'Ciao come stai?',
+        },
+        {
+            name: 'Samuele',
+            avatar: 'img/avatar_3.jpg',
+            lastMessage: 'Ciao che stai facendo?',
+        },
+        {
+            name: 'Alessandro B.',
+            avatar: 'img/avatar_4.jpg',
+            lastMessage: 'Come hai fatto?',
+        },
+        
+      ],
+    },
+  });
+  
